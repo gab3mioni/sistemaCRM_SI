a13dae2c6a1501282b68547880b115bf8bbfea12
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-04-05 11:22:59 -0300

    db

--- app/database/sistemaCRM_SI.sql
+++ app/database/sistemaCRM_SI.sql
@@ -4,20 +4,20 @@ USE `sistemaCRM`;
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin`(
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(45) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`idaprovacao`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(45) NOT NULL,
-  `nome` varchar(45) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `razao` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `cpf` varchar(45) NOT NULL,
+  `cnpj` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-)
+  PRIMARY KEY (`id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
