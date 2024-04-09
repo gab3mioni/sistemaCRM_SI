02ea2d136df1c7295d211c30298a4032a2dd3c23
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-04-09 15:21:53 -0300

    Atualização Database
    > Alterado tabela de admin
    > Alterado tabela de clientes
    > Adicionado tabela de vendedores

--- app/database/sistemaCRM_SI.sql
+++ app/database/sistemaCRM_SI.sql
@@ -6,18 +6,26 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin`(
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(45) NOT NULL,
-  `nome` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
+DROP TABLE IF EXISTS `vendedores`;
+CREATE TABLE IF NOT EXISTS `vendedores` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `razao` varchar(45) NOT NULL,
+  `usuario` varchar(45) NOT NULL,
+  `nome` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `cnpj` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `cpf` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS  `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `razao` varchar(45) NOT NULL,
+  `cnpj` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
