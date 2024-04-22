9cbf4f719c4fe433386cd306c330e04e5783e888
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-04-22 15:32:23 -0300

    redirecionamento dashboard

--- public/dashboard.php
+++ public/dashboard.php
@@ -32,7 +32,7 @@ session_start();
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Clientes</a>
+                        <a class="nav-link" aria-current="page" href="clientes.php">Clientes</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Vendedores</a>
