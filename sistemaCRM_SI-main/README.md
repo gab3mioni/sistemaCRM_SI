2f84d04f33ebb576d72315025a74504cb65b8397
MelissaMaximo <129124909+MelissaMaximo@users.noreply.github.com>
2024-04-26 13:39:20 -0300

    Add files via upload

---
+++ sistemaCRM_SI-main/README.md
@@ -0,0 +1,2 @@
+# sistemaCRM_SI
+ Software CRM desenvolvida em PHP para um trabalho de Sistemas da Informação, no curso de Análise e Desenvolvimento de Sistemas, da FATEC Guaratinguetá - Prof. João Mod
---
+++ sistemaCRM_SI-main/config/auth.php
@@ -0,0 +1,36 @@
+<?php
+function conectarDB() {
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'sistemacrm';
+
+    try {
+        $conexao = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conexao;
+    } catch (PDOException $e) {
+        die("Erro de conexão com o banco de dados: " . $e->getMessage());
+    }
+}
+
+function verificarCredenciais($usuario, $senha) {
+    try {
+        $conexao = conectarDB();
+
+        $query = "SELECT id FROM vendedores WHERE usuario = :usuario AND senha = :senha";
+        $stmt = $conexao->prepare($query);
+        $stmt->bindParam(':usuario', $cnpj);
+        $stmt->bindParam(':usuario', $senha);
+        $stmt->execute();
+
+        if ($stmt->rowCount() == 1) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $row['id'];
+        } else {
+            return false;
+        }
+    } catch (PDOException $e) {
+        die("Erro ao verificar credenciais: " . $e->getMessage());
+    }
+}
