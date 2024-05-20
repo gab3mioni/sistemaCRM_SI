d35a13bb43249195d0fddb698428b04764a3e9f5
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-05-20 17:35:44 -0300

    Correções visuais

--- public/clientes.php
+++ public/clientes.php
@@ -1,5 +1,5 @@
 <?php
-include("../config/config.php");
+include ("../config/config.php");
 
 session_start();
 
@@ -15,12 +15,12 @@ $stmt->execute();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../app/class/clientes.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-       
-    </style>
+
+    <link rel="stylesheet" href="../app/class/table/table.css">
+    <link rel="stylesheet" href="../app/class/style.css">
 </head>
 
 <body>
@@ -33,21 +33,26 @@ $stmt->execute();
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
+
                 <span class="navbar-toggler-icon"></span>
+
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="dashboard.php">Etapas de vendas</a>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="">Gráficos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="vendas.php">Vendas</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="clientes.php">Clientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="vendedores.php">Vendedores</a>
+                        <a class="nav-link" aria-current="page" href="vendedores.php">Vendedores</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="configuracoes.php">Configurações</a>
+                        <a class="nav-link disabled" href="configuracoes.php">Configurações</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Sair</a>
@@ -91,7 +96,7 @@ $stmt->execute();
         </table>
     </div>
 
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
--- public/dashboard.php
+++ public/dashboard.php
@@ -1,13 +1,3 @@
-<?php
-include ("../config/config.php");
-
-session_start();
-
-$consultaCards = "SELECT * FROM vendas ORDER BY id ASC";
-$stmt = $conexao->prepare($consultaCards);
-$stmt->execute();
-?>
-
 
 
 <!DOCTYPE html>
@@ -20,7 +10,7 @@ $stmt->execute();
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../app/class/clientes.css">
+    <link rel="stylesheet" href="../app/class/style.css">
 
 </head>
 
@@ -35,11 +25,17 @@ $stmt->execute();
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
 
-                <span class="navbar-toggler-icon"></span>]
+                <span class="navbar-toggler-icon"></span>
 
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="">Gráficos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="vendas.php">Vendas</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="clientes.php">Clientes</a>
                     </li>
@@ -58,48 +54,9 @@ $stmt->execute();
     </nav>
     <!-- fim cabeçalho -->
 
-    <div class="search">
-        <form>
-            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" id="searchInput">
-        </form>
-    </div>
-
-    <div class="col-12">
-        <table class="content-table small-screen">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Razão Social</th>
-                    <th scope="col">CNPJ</th>
-                    <th scope="col">Itens</th>
-                    <th scope="col">Quantidade</th>
-                    <th scope="col">Valor</th>
-                    <th scope="col">Dta</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                while ($userData = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<tr>";
-                    echo "<td>" . $userData['id'] . "</td>";
-                    echo "<td>" . $userData['razao'] . "</td>";
-                    echo "<td>" . $userData['cnpj'] . "</td>";
-                    echo "<td>" . $userData['itens'] . "</td>";
-                    echo "<td>" . $userData['quantidade'] . "</td>";
-                    echo "<td>" . $userData['valor'] . "</td>";
-                    echo "<td>" . $userData['data'] . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
-
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="../app/js/dashboard.js"></script>
 </body>
 
 </html>
