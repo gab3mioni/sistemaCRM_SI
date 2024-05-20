531f7167b68872e6b1ef95b4129422609542fb9a
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-05-20 15:53:36 -0300

    Correção css clientes.php

--- public/clientes.php
+++ public/clientes.php
@@ -15,7 +15,7 @@ $stmt->execute();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="clientes.css">
+    <link rel="stylesheet" href="../app/class/clientes.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
